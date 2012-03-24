689a34bb7998f87b7d82ee4007c04799deb6f618
Brian M. Scott <brianscott0017@yahoo.com>
2012-03-24 15:03:29 -0400

    303

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,16 @@
 class PagesController < ApplicationController
 
   def wordswithnofriends
-    @title = "Words With No Friends"
-    if params.size <3 then
-      @results = ''
-      @letters = ['w','','r','d']
-    else
+    redirect_to "pages/wordswithnofriends"
+    #@title = "Words With No Friends"
+    #if params.size <3 then
+     # @results = ''
+      #@letters = ['w','','r','d']
+    #else
       #puts params.size
-     @results = WordsDictionary.find_words(params[:word], params[:options])
-     @letters = params[:word]
-    end
+     #@results = WordsDictionary.find_words(params[:word], params[:options])
+     #@letters = params[:word]
+    #end
   end
 
 end
